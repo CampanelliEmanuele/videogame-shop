3f48b077419938c0c4146b4ef150a65fc016c32f
CampanelliEmanuele <emanuele.campanelli.work@gmail.com>
2024-01-24 15:12:28 +0100

    Add: Create feature of CRUD for Videogame

--- src/main/java/org/learning/videogameshop/controller/VideogameController.java
+++ src/main/java/org/learning/videogameshop/controller/VideogameController.java
@@ -1,16 +1,21 @@
 package org.learning.videogameshop.controller;
 
+import jakarta.validation.Valid;
 import org.learning.videogameshop.model.Videogame;
 import org.learning.videogameshop.repository.PurchaseRepository;
 import org.learning.videogameshop.repository.TypeRepository;
 import org.learning.videogameshop.repository.VideogameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/videogames")
@@ -29,5 +34,26 @@ public class VideogameController {
         model.addAttribute("videogameList", videogameList);
         return "/videogames/list";
     }
+
+    @GetMapping("/create")
+    public String create(Model model) {
+        Videogame recipe = new Videogame();
+        model.addAttribute("videogame", recipe);
+//        model.addAttribute("typeList", typeRepository.findAll());
+        return "videogames/create";
+    }
+
+    @PostMapping("/create")
+    public String store(@Valid @ModelAttribute("videogame") Videogame videogameForm, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+//            model.addAttribute("typeList", typeRepository.findAll());
+            System.out.println(bindingResult.getAllErrors());
+            return "videogames/create";
+        }
+        Videogame savedVideogame = videogameRepository.save(videogameForm);
+        return "redirect:/videogames/show/" + savedVideogame.getId();
+    }
+
+
 
 }
