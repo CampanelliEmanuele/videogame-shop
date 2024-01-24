d47ea3c6aefc899cd0b5a2b650cf952a9a2ed631
CampanelliEmanuele <emanuele.campanelli.work@gmail.com>
2024-01-24 12:32:37 +0100

    Reformat: class renamed

--- src/main/java/org/learning/videogameshop/model/Videogame.java
+++ src/main/java/org/learning/videogameshop/model/Videogame.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 @Entity
 @Table(name = "videogames")
-public class VideoGame {
+public class Videogame {
 
     @Autowired
     private VideogameRepository videogameRepository;
