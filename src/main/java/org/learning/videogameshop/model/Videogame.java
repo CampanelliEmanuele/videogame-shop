ff55887307aebb627e1e89f68bc8384f8d8f4555
CampanelliEmanuele <emanuele.campanelli.work@gmail.com>
2024-01-24 12:23:38 +0100

    Update: Videogame.class

--- src/main/java/org/learning/videogameshop/model/Videogame.java
+++ src/main/java/org/learning/videogameshop/model/Videogame.java
@@ -1,16 +1,76 @@
 package org.learning.videogameshop.model;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 
 @Entity
 @Table(name = "videogames")
 public class Videogame {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-    private String description;
-    private String photo;
+    @NotEmpty(message = "Name must not be blank")
+    @Size(min = 2, max = 40, message = "Name size must be from 2 to 40 characters")
+    @Column(nullable = false)
+    private String name;
+
+    @NotEmpty(message = "Videogame must have a price")
+    @Size(min = 2, max = 40)
     private Double price;
+    private String description;
+    @Lob
+    private String photo;
+
+    @OneToOne
+    private Purchase purchase;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Purchase getPurchase() {
+        return purchase;
+    }
+
+    public void setPurchase(Purchase purchase) {
+        this.purchase = purchase;
+    }
 }
