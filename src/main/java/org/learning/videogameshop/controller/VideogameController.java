30a9ddbe63648fbe2d3ba66f3fee325286bacc16
CampanelliEmanuele <emanuele.campanelli.work@gmail.com>
2024-01-24 12:47:37 +0100

    Add: Read feature of CRUD for Videogame

--- src/main/java/org/learning/videogameshop/controller/VideogameController.java
+++ src/main/java/org/learning/videogameshop/controller/VideogameController.java
@@ -1,11 +1,16 @@
 package org.learning.videogameshop.controller;
 
+import org.learning.videogameshop.model.Videogame;
 import org.learning.videogameshop.repository.PurchaseRepository;
 import org.learning.videogameshop.repository.TypeRepository;
 import org.learning.videogameshop.repository.VideogameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/videogames")
@@ -17,5 +22,12 @@ public class VideogameController {
     private TypeRepository typeRepository;
     @Autowired
     private PurchaseRepository purchaseRepository;
+
+    @GetMapping
+    public String index(Model model) {
+        List<Videogame> videogameList = videogameRepository.findAll();
+        model.addAttribute("videogameList", videogameList);
+        return "/videogames/list";
+    }
 
 }
