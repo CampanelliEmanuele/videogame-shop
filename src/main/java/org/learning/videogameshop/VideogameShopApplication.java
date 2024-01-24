f9f7200f55f2ebbc0fd50bb4ad63793a6d3c98ce
CampanelliEmanuele <emanuele.campanelli.work@gmail.com>
2024-01-24 10:53:49 +0100

    Initial commit

---
+++ src/main/java/org/learning/videogameshop/VideogameShopApplication.java
@@ -0,0 +1,13 @@
+package org.learning.videogameshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideogameShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideogameShopApplication.class, args);
+	}
+
+}
