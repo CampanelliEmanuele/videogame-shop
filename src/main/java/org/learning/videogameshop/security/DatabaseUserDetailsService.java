576adc3953b89d366dea07b82fdac5533c800652
CampanelliEmanuele <emanuele.campanelli.work@gmail.com>
2024-01-30 14:45:01 +0100

    Add: security classes

---
+++ src/main/java/org/learning/videogameshop/security/DatabaseUserDetailsService.java
@@ -0,0 +1,32 @@
+package org.learning.videogameshop.security;
+
+import org.learning.videogameshop.model.LibraryUser;
+import org.learning.videogameshop.repository.LibraryUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class DatabaseUserDetailsService implements UserDetailsService {
+
+  @Autowired
+  private LibraryUserRepository repository;
+
+  @Override
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    // cerco il LibraryUser con l'email = a quello username
+    Optional<LibraryUser> libraryUser = repository.findByEmail(username);
+    // se lo trovo lo converto in un DatabaseUserDetail e lo restituisco
+    if (libraryUser.isPresent()) {
+      return new DatabaseUserDetails(libraryUser.get());
+    } else {
+      // se non lo trovo sollevo un'eccezione UsernameNotFoundException
+      throw new UsernameNotFoundException(username);
+    }
+  }
+
+}
