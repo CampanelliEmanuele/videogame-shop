7e23fc319a522de0bbf46f36797d2c837084d9aa
CampanelliEmanuele <emanuele.campanelli.work@gmail.com>
2024-02-02 00:22:29 +0100

    Reformat&Update: code cleanup&add a secondary navbar + footer

--- src/main/java/org/learning/videogameshop/controller/HomeController.java
+++ src/main/java/org/learning/videogameshop/controller/HomeController.java
@@ -1,17 +1,10 @@
 package org.learning.videogameshop.controller;
 
-import org.learning.videogameshop.model.Purchase;
-import org.learning.videogameshop.repository.PurchaseRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.temporal.ChronoUnit;
-import java.util.*;
 
 @Controller
 @RequestMapping("/")
