7c1bff88b3af24905f9ca338cf696b1215d0c917
CampanelliEmanuele <emanuele.campanelli.work@gmail.com>
2024-02-01 12:10:45 +0100

    Update: backend RUD (no Create) for Purchase are now available

--- src/main/java/org/learning/videogameshop/controller/PurchaseController.java
+++ src/main/java/org/learning/videogameshop/controller/PurchaseController.java
@@ -1,15 +1,21 @@
 package org.learning.videogameshop.controller;
 
+import jakarta.validation.Valid;
 import org.learning.videogameshop.model.Purchase;
 import org.learning.videogameshop.repository.PurchaseRepository;
 import org.learning.videogameshop.repository.VideogameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/purchases")
@@ -28,6 +34,19 @@ public class PurchaseController {
         model.addAttribute("purchaseList", purchaseList);
         return "purchases/list";
     }
+
+    @GetMapping("/show/{id}")
+    public String show(@PathVariable Integer id, Model model) {
+        Optional<Purchase> result = purchaseRepository.findById(id);
+        if (result.isPresent()) {
+            model.addAttribute("purchase", result.get());
+            return "purchases/show";
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Purchase with id " + id + " not found");
+        }
+
+    }
+
 //    @GetMapping("/create")
 //    //   public String create(Model model) {
 //    public String create(@RequestParam(name = "videogameId", required = false) Integer videogameId, Model model) {
@@ -59,57 +78,44 @@ public class PurchaseController {
 //        return "redirect:/purchases";
 //    }
 //
-//    @GetMapping("/edit/{id}")
-//    public String edit(@PathVariable Integer id, Model model) {
-//        Optional<Purchase> result = purchaseRepository.findById(id);
-//        if (result.isPresent()) {
-//            model.addAttribute("purchase", result.get());
-//            return "purchases/edit";
-//        } else {
-//            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Purchase with id " + id + " not found");
-//        }
-//    }
-//
-//    @PostMapping("/edit/{id}")
-//    public String update(@PathVariable Integer id, @Valid @ModelAttribute("purchase") Purchase stockFrom,
-//                         BindingResult bindingResult) {
-//        Optional<Purchase> result = purchaseRepository.findById(id);
-//        if (result.isPresent()) {
-//            if (bindingResult.hasErrors()) {
-//                System.out.println(bindingResult.getAllErrors());
-//                return "purchases/edit";
-//            }
-//            Purchase savedRecipe = purchaseRepository.save(stockFrom);
-//            return "redirect:/purchases/show/" + id;
-//        } else {
-//            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Purchase with id " + id + " not found");
-//        }
-//    }
-//
-//    @PostMapping("/delete/{id}")
-//    public String delete(@PathVariable Integer id, RedirectAttributes redirectAttributes) {
-//        Optional<Purchase> result = purchaseRepository.findById(id);
-//        if (result.isPresent()) {
-//            purchaseRepository.deleteById(id);
-//            redirectAttributes.addFlashAttribute("redirectMessage",
-//                    "Purchase with id " + result.get().getId() + " deleted!");
-//            return "redirect:/purchases";
-//        } else {
-//            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Purchase with id " + id + " not found");
-//        }
-//    }
-//
-//    @GetMapping("/show/{id}")
-//    public String show(@PathVariable Integer id, Model model) {
-//        Optional<Purchase> result = purchaseRepository.findById(id);
-//        if (result.isPresent()) {
-//            model.addAttribute("purchase", result.get());
-//
-//            return "purchases/show";
-//        } else {
-//            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Purchase with id " + id + " not found");
-//        }
-//
-//    }
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable Integer id, Model model) {
+        Optional<Purchase> result = purchaseRepository.findById(id);
+        if (result.isPresent()) {
+            model.addAttribute("purchase", result.get());
+            return "purchases/edit";
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Purchase with id " + id + " not found");
+        }
+    }
+
+    @PostMapping("/edit/{id}")
+    public String update(@PathVariable Integer id, @Valid @ModelAttribute("purchase") Purchase stockFrom,
+                         BindingResult bindingResult) {
+        Optional<Purchase> result = purchaseRepository.findById(id);
+        if (result.isPresent()) {
+            if (bindingResult.hasErrors()) {
+                System.out.println(bindingResult.getAllErrors());
+                return "purchases/edit";
+            }
+            Purchase savedRecipe = purchaseRepository.save(stockFrom);
+            return "redirect:/purchases";
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Purchase with id " + id + " not found");
+        }
+    }
+
+    @PostMapping("/delete/{id}")
+    public String delete(@PathVariable Integer id, RedirectAttributes redirectAttributes) {
+        Optional<Purchase> result = purchaseRepository.findById(id);
+        if (result.isPresent()) {
+            purchaseRepository.deleteById(id);
+            redirectAttributes.addFlashAttribute("redirectMessage",
+                    "Purchase with id " + result.get().getId() + " deleted!");
+            return "redirect:/purchases";
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Purchase with id " + id + " not found");
+        }
+    }
 
 }
